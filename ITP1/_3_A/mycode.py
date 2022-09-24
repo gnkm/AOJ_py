f7f60d7574dc3c549938acd4a2bb8d4b55d9467c
gnkm <genki.matsunaga@gmail.com>
2022-09-24 16:41:36 +0900

    Add ITP1 3_A

---
+++ ITP1/_3_A/mycode.py
@@ -0,0 +1,15 @@
+"""
+Print Many Hello World
+
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/3/ITP1_3_A
+"""
+
+import sys
+
+def main():
+    for i in range(1_000):
+        print('Hello World')
+
+
+if __name__ == '__main__':
+    main()
