b1c94936c92559cf8a73e5375a8a43c9923c67ff
gnkm <genki.matsunaga@gmail.com>
2022-09-24 17:02:13 +0900

    Add ITP1 3_B

---
+++ ITP1/_3_B/mycode.py
@@ -0,0 +1,27 @@
+"""
+Print Test Cases
+
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/3/ITP1_3_B
+"""
+
+import sys
+
+input = lambda: sys.stdin.readline().rstrip('\r\n').split()
+
+
+def main():
+    # x = input()  # => ['3']
+    # x = map(int, input())  # => <map object at 0x1016533d0>
+    # [x] = map(int, input())  # => 3
+    i = 1
+    while True:
+        [num] = map(int, input())
+        if num == 0:
+            sys.exit()
+
+        print(f'Case {i}: {num}')
+        i += 1
+
+
+if __name__ == '__main__':
+    main()
