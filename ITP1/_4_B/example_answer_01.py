5d19e67d3c69f0e80f97b2d7a049dbeae4981ed6
gnkm <genki.matsunaga@gmail.com>
2022-10-17 17:41:32 +0900

    ITP1 4B: Add an example answer

---
+++ ITP1/_4_B/example_answer_01.py
@@ -0,0 +1,12 @@
+"""
+Circle
+
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/4/ITP1_4_B
+
+Result: AC
+"""
+
+
+import math
+r = float(input())
+print(f'{math.pi*r*r:.12f} {2*math.pi*r:.12f}')
