b1e0ed2751d9aef2749e611c0b312cb4902a59bb
gnkm <genki.matsunaga@gmail.com>
2024-03-12 23:49:32 +0900

    ALDS1_04C: Add my code

---
+++ aoj/ALDS1/_04_C/mycode.py
@@ -0,0 +1,35 @@
+"""ALDS1_4_C < Lesson < Courses | Aizu Online Judge
+https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_4_C
+
+Result: WA
+"""
+
+
+import sys
+
+
+input = lambda: sys.stdin.readline().rstrip("\r\n").split()
+i_sesli = lambda: int(input()[0])
+i_mesls = lambda: list(input())
+i_memls = lambda n: [i_mesls() for _ in range(n)]
+
+
+def main():
+    N = i_sesli()
+    queries = i_memls(N)
+    strings = ""
+    for query in queries:
+        operation = query[0]
+        query_string = query[1]
+        if operation == "insert":
+            strings += query_string
+        elif operation == "find":
+            if query_string in strings:
+                print("yes")
+            else:
+                print("no")
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
